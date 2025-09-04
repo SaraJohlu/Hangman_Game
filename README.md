a009c2356aa7935907eb3afd821af872b09b9033
Sara <sara_lunden1@hotmail.com>
2025-09-04 11:32:31 +0200

    continued building on readme and on functions

--- README.md
+++ README.md
@@ -8,8 +8,22 @@ First of all we want to decide a game state for our game, in this case we have 3
 With <b>ENUM</b> we can put those states like this. 
 
 enum gameState {
-  playing = "PLAYING",
-  won = "WON",
-  lost = "LOST", 
+  Playing,
+  Won,
+  Lost, 
 };
+
+With this we can change the different game states to our functions we will build. 
+Like for exampel, gameState.Playing, gameState.Won, gameState.Lost. <i>How these will look inside of function will be displayed further down on thi page.</i> 
+
+Lets continue to the <b>Literal Types</b>.
+In our case as we build a hangman game, we can with types decide which letters that will be "right" to use.
+
+exampel for this on our code is:
+
+type Letter =
+  | "a" | "b" | "c" | "d" | "e" | "f" | "g"
+  | "h" | "i" | "j" | "k" | "l" | "m" | "n"
+  | "o" | "p" | "q" | "r" | "s" | "t" | "u"
+  | "v" | "w" | "x" | "y" | "z";
 
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,17 @@ import './style.css'
 
 // Creating the different states in the game using enum.
 enum gameState {
-  playing = "PLAYING",
-  won = "WON",
-  lost = "LOST", 
-};
+  Playing,
+  Won,
+  Lost,
+}
 
 // using Literal types for all the letters that can be used in the game.
-type Letter = {
-  a: "A", b:"B", c:"C", d:"D", e:"E", 
-  f:"F", g:"G", h:"H", i:"I", j:"J", 
-  k:"K", l:"L", m:"M", n:"N", o:"O", 
-  p:"P", q:"Q", r:"R", s:"S", t:"T", 
-  u:"U", v:"V", w:"W", x:"X", y:"Y", 
-  z:"Z"
-};
+type Letter =
+  | "a" | "b" | "c" | "d" | "e" | "f" | "g"
+  | "h" | "i" | "j" | "k" | "l" | "m" | "n"
+  | "o" | "p" | "q" | "r" | "s" | "t" | "u"
+  | "v" | "w" | "x" | "y" | "z";
 
 // Interface for the game 
 
@@ -27,33 +24,31 @@ interface hangman_game {
 };
 
 // Function to start the game, takes a word as an argument and returns an object of type hangman_game.
-function start_hangman (word: string): hangman_game {
+function start_hangman(word: string): hangman_game {
   return {
     word: word.toLowerCase(),
     guessed_letters: [],
     max_fails: 6,
-    state: gameState.playing
+    state: gameState.Playing
   }
 };
 
-function guess_letter(game: hangman_game, letter: Letter): hangman_game{
-  if (game.state !== gameState.playing) return game; // If the gamestate is not playing return game as it is, "game is over".
+function guess_letter(game: hangman_game, letter: Letter): hangman_game {
+  if (game.state !== gameState.Playing) return game; // If the gamestate is not playing return game as it is, "game is over".
 
-  if(!game.guessed_letters.includes(letter)) {
+  if (!game.guessed_letters.includes(letter)) {
     game.guessed_letters.push(letter); // If the letter has not been guessed, the program adds the letter into the guessed_letters array.
   }
 
   const fail = game.guessed_letters.filter(l => !game.word.includes(l)).length; // Count the number of failed guesses. 
   /*If the guessed letter is not in the word it will increment the fail count by filter through the array for the set word*/
 
-  if(fail >= game.max_fails) {
-    game.state = gameState.lost;  
+  if (fail >= game.max_fails) {
+    game.state = gameState.Lost;
   }
-  else if (game.word.split("").every(l => game.guessed_letters.includes(l))){
-    game.state = gameState.won;
+  else if (game.word.split("").every(l => game.guessed_letters.includes(l as Letter))) {
+    game.state = gameState.Won;
   }
   return game;
-
-
 }
 
